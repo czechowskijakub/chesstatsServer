aecccbdb03a9f0fef7504bbf1009c057a18653be
Jakub Czechowski <kubaczech911@gmail.com>
2026-05-27 22:36:43 +0200

    urls updated for games' history

--- chessServer/chessBackend/urls.py
+++ chessServer/chessBackend/urls.py
@@ -6,5 +6,6 @@ from django.http import JsonResponse
 urlpatterns = [
     path('chessBackend/', views.hello, name='hello'),
     path('stats/<str:username>/', views.get_chess_api, name='get_stats'),
-    path('mostplayed/<str:username>/', views.get_openings_by_result, name='get_openings')
+    path('mostplayed/<str:username>/', views.get_openings_by_result, name='get_openings'),
+    path('games/<str:username>/', views.get_month_games, name='get_games'),
 ]
